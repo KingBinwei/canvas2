59154c8bec6fc5c7b2c12a8c1154b4f81a62652c
KingBinwei <k19990101@icloud.com>
2019-01-21 17:40:23 +0800

    Next project

--- javascripts/canvas.js
+++ javascripts/canvas.js
@@ -1,30 +1,19 @@
-var c = document.querySelector('#drawing');
-var ctx = c.getContext("2d");
-var image = new Image();
-image.onload = function() {
-  ctx.drawImage(image, 0, 0);
-}
+var canvas = document.querySelector("canvas");
+var ctx = canvas.getContext('2d');
+document.addEventListener('DOMContentLoaded', function() {
+ 	var v = document.querySelector('#v');
+ 	var canvas = document.querySelector('#drawing');
+ 	var ctx = canvas.getContext("2d");
 
-image.src = "images/palette.jpg";
-var makePixelGrayScale = function (r, g, b, a) {
-	var y = (0.3 * r) + （0.59 * g） + (0.11 * b);
-	return {r:y, g:y, b:y, a:y};
-}
-
-function makeGrayScale() {
-	var r, g, b, a;
-	var imageData = ctx.getImageData(0, 0, 300, 300);
-	var numPixels = imageData.data.length/4;
-	for (var i = 0; i < numPixels; i++) {
-		r = imageData.data[i * 4 + 0];
-		g = imageData.data[i * 4 + 1];
-		b = imageData.data[i * 4 + 2];
- 		a = imageData.data[i * 4 + 3];
- 		pixel = makePixelGrayScale(r, g, b, a);
- 		imageData.data[i * 4 + 0] = pixel.r;
- 		imageData.data[i * 4 + 1] = pixel.g;
- 		imageData.data[i * 4 + 2] = pixel.b;
- 		imageData.data[i * 4 + 3] = pixel.a;
-	}
-	ctx.putImageDate(imageData,0 , 0);
-}
+ 	v.addEventListener('loadedmetadata', function() {
+ 		canvas.width = this.videoWidth;
+ 		canvas.height = this.VideoHeight;
+ 	});
+ 	var draw = function() {
+ 		canvas.getContext("2d").drawImage(v, 0, 0);
+ 	}
+ 	v.addEventListener('play', function() {
+ 		if (v.paused || v.ended) return;
+ 		draw();
+ 	});
+ });
